dac46df80a6abb0c62c028980a2e1c526627b2e0
pflevy <omega6.pc@gmail.com>
2020-08-22 14:26:24 -0300

    Selectable image through input

--- public/loadImage.js
+++ public/loadImage.js
@@ -1,1 +1,7 @@
 const image = document.getElementById("image");
+
+document.getElementById("imageInput").onchange = function (e) {
+  image.onload = renderElements;
+  image.onerror = () => console.log("Error loading image");
+  image.src = URL.createObjectURL(this.files[0]);
+};
