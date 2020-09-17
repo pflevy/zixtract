7eda5a9b61ba0bdc0d67b3401de6b4a3fb6cbcdf
pflevy <omega6.pc@gmail.com>
2020-09-17 18:12:13 -0300

    Updated layout and started working on New Rule functionality.

---
+++ public/conditionalRules.js
@@ -0,0 +1,14 @@
+const newRoleNode = document.getElementById("newRule");
+const rightSideNode = document.getElementsByClassName("rightSideContent")[0];
+newRoleNode.onclick = (e) => {
+  const selectList = document.createElement("select");
+  console.log(elements);
+  elements.forEach((el) => {
+    console.log(el);
+    var option = document.createElement("option");
+    option.value = el.extractedText;
+    option.text = el.id + " " + el.extractedText;
+    selectList.appendChild(option);
+  });
+  rightSideNode.appendChild(selectList);
+};
